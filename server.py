ff910f28f91314fec600a5ba3077a169a3690199
Ingrid Aldum <iald7453@SHARED.SYDNEY.EDU.AU>
2019-01-12 11:41:33 +1100

    integrated petbot with flask

--- server.py
+++ server.py
@@ -1,14 +1,34 @@
 from flask import Flask, request, jsonify
 import requests
+import findapet
+
+state = 'NO_QUERY'
+context = {}
+
 
 app = Flask(__name__)
 
 # A basic homepage, to check everything is working.
 @app.route('/',  methods=['POST', 'GET'])
 def index():
-    message = request.form.get('text')
-    return f"You said {message}"
+    line = request.form.get('text')
+ 
+    ret = findapet.ON_INPUT[state](line, context) 
 
+    state, context, optional_output = ret
+    if optional_output:
+        #
+        return jsonify({
+        'reponse_type': 'in_channel',
+        'text': optional_output
+    })
+    
+    output = findapet.ON_ENTER_STATE[state](context)
+     return jsonify({
+        'reponse_type': 'in_channel',
+        'text': output
+    })
+    
 
 # A basic slack endpoint.
 @app.route('/mycommand', methods=['POST'])
